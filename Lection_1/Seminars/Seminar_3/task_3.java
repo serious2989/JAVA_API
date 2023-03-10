f296d061fb3d7c11774805fe7798a1d8a4a152d1
Dmitriy <serious2989@gmail.com>
2023-03-10 16:45:25 +0300

    task_1_buble_sort_HW

--- Lection_1/Seminars/Seminar_3/task_3.java
+++ Lection_1/Seminars/Seminar_3/task_3.java
@@ -1,13 +1,12 @@
 // Создать список типа ArrayList. Поместить в него как строки, так и целые числа.
 //  Пройти по списку, найти и удалить целые числа.
 
-
 package Seminar_3;
 
 import java.util.ArrayList;
 
 public class task_3 {
-    static void ex3(){
+    static void ex3() {
         ArrayList list = new ArrayList<>();
         list.add("asdad5");
         list.add("asdad4");
@@ -20,8 +19,8 @@ public class task_3 {
         list.add(22);
 
         System.out.println(list);
-        for (int i = list.size()-1; i >= 0; i--) {
-            if (list.get(i) instanceof Integer){
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (list.get(i) instanceof Integer) {
                 list.remove(i);
 
             }
