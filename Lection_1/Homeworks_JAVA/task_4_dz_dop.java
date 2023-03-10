2ad79d0f145a4c03c1b2b34ede6e037d855a44f3
Dmitriy <serious2989@gmail.com>
2023-03-10 10:22:07 +0300

    dz_4_dop_in seminar_1

---
+++ Lection_1/Homeworks_JAVA/task_4_dz_dop.java
@@ -0,0 +1,21 @@
+package Lection_1.Homeworks_JAVA;
+
+import java.util.Scanner;
+
+public class task_4_dz_dop {
+    public static void main(String[] args) {
+
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Enter equation like '2? + ?5 = 69': ");
+        String equation = iScanner.nextLine(); //2? + ?5 = 69.
+        String message = "Решения нет!";
+        for (int i = 0; i < 10; i++) {
+            String result = equation.replace("?", Integer.toString(i));
+            // 20 + 05 = 69  
+            String[] arr = result.split(" ");
+            if (Integer.parseInt(arr[0]) + Integer.parseInt(arr[2]) == Integer.parseInt(arr[4]))
+                message = result;
+        }
+        System.out.println(message);
+    }
+}
