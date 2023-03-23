fe94a53addb26b829c7fcc7e6dd6eaca98ad37f9
Dmitriy <serious2989@gmail.com>
2023-03-23 11:04:03 +0300

    Notebooks_HW_6

--- Lection_1/Seminars/Seminar_3/task_1.java
+++ Lection_1/Seminars/Seminar_3/task_1.java
@@ -36,19 +36,19 @@ import java.util.logging.SimpleFormatter;
 public class task_1 {
 
 public static void main(String[] args) throws IOException{
-    Logger ll = Logger.getLogger(lg.class.getName());
-    String logsPath = "log.txt";
-    FileHandler fh = new FileHandler(logsPath, false);
-    ll.addHandler(fh);
-    SimpleFormatter formatter = new SimpleFormatter();
-    // XMLFormatter form = new XMLFormatter();
-    fh.setFormatter(formatter);
+//     // Logger ll = Logger.getLogger(lg.class.getName());
+//     String logsPath = "log.txt";
+//     FileHandler fh = new FileHandler(logsPath, false);
+//     // ll.addHandler(fh);
+//     SimpleFormatter formatter = new SimpleFormatter();
+//     // XMLFormatter form = new XMLFormatter();
+//     fh.setFormatter(formatter);
 
-    ll.log(Level.INFO,"element: " + i);
-    ll.log(Level.WARNING, "logger off" );
+//     ll.log(Level.INFO,"element: " + i);
+//     ll.log(Level.WARNING, "logger off" );
 
-    ll.info("test" );
-    ll.warning("logsPath");
+//     ll.info("test" );
+//     ll.warning("logsPath");
 
-    }
+}
 }
